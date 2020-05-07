0312a3374a536bf047a640ec5a2b302396a320d0
dl155231 <61503932+dl155231@users.noreply.github.com>
2020-05-07 12:17:56 +0200

    Create zad04.py

---
+++ wd9/zad04.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# wczytanie danych datasetu
+iris = pd.read_csv('iris.csv', sep=',')
+
+# legenda wykresu
+colors = ['red', 'green', 'blue']
+gatunki = iris['species'].unique()
+
+
+for i in range(0, 3):
+    species_df = iris[iris['species'] == gatunki[i]]
+    plt.scatter(
+        species_df['sepal_length'],
+        species_df['sepal_width'],
+        color=colors[i],
+        alpha=0.7,
+        label=gatunki[i]
+    )
+
+plt.xlabel('sepal length (cm)')
+plt.ylabel('sepal width (cm)')
+plt.legend()
+
+plt.show()
